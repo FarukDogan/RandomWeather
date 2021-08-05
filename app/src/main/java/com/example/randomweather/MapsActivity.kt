4b5555520a5bc30ec135f253650c718207f04289
Faruk Dogan <82907943+FarukDogan@users.noreply.github.com>
2021-08-05 20:21:16 +0300

    Update MapsActivity.kt

--- app/src/main/java/com/example/randomweather/MapsActivity.kt
+++ app/src/main/java/com/example/randomweather/MapsActivity.kt
@@ -4,6 +4,7 @@ import android.Manifest
 import android.annotation.SuppressLint
 import android.content.Context
 import android.content.Intent
+import android.content.SharedPreferences
 import android.content.pm.PackageManager
 import android.location.Geocoder
 import android.location.Location
@@ -32,9 +33,7 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
     private lateinit var mMap: GoogleMap
     private lateinit var locationManager : LocationManager
     private lateinit var locationListener: LocationListener
-
-
-
+   // lateinit var sharedPreferences: SharedPreferences
 
 
     override fun onCreateOptionsMenu(menu: Menu?): Boolean {
@@ -48,11 +47,11 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
 
             val randomlat=(0..90).random().toDouble()
             val randomlong=(-125..179).random().toDouble()
-            val latUzantı = ("$randomlat"+"1")
-            val lonUzantı = ("$randomlong"+"1")
+            val latUzanti = ("$randomlat"+"1")
+            val lonUzanti = ("$randomlong"+"1")
 
 
-            val denkgelenlokasyon=LatLng(latUzantı.toDouble(),lonUzantı.toDouble())
+            val denkgelenlokasyon=LatLng(latUzanti.toDouble(),lonUzanti.toDouble())
 
             yenile(denkgelenlokasyon)
 
@@ -69,6 +68,7 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
         setContentView(R.layout.activity_maps)
 
 
+
         val mapFragment = supportFragmentManager
             .findFragmentById(R.id.map) as SupportMapFragment
         mapFragment.getMapAsync(this)
@@ -78,59 +78,59 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
     override fun onMapReady(googleMap: GoogleMap) {
 
 // mapimiz açıldıgında kullanıcın güncel konumu veyyahut son konumunu gösterdiğimiz kodlarımız
-     mMap = googleMap
-     mMap.setOnMapLongClickListener(dinleyici)
+        mMap = googleMap
+        mMap.setOnMapLongClickListener(dinleyici)
 
-     locationManager = getSystemService(Context.LOCATION_SERVICE) as LocationManager
-     locationListener = object : LocationListener {
-         override fun onLocationChanged(p0: Location) {
+        locationManager = getSystemService(Context.LOCATION_SERVICE) as LocationManager
+        locationListener = object : LocationListener {
+            override fun onLocationChanged(p0: Location) {
 
-             mMap.clear()
-             val guncelKonum = LatLng(p0.latitude, p0.longitude)
+                mMap.clear()
+                val guncelKonum = LatLng(p0.latitude, p0.longitude)
 
-             println("${p0.latitude}" + "${p0.longitude}")
+                println("${p0.latitude}" + "${p0.longitude}")
 
-             mMap.addMarker(MarkerOptions().position(guncelKonum).title("Guncel Konumunuz"))
-             mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(guncelKonum, 12f))
+                mMap.addMarker(MarkerOptions().position(guncelKonum).title("Guncel Konumunuz"))
+                mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(guncelKonum, 12f))
 
-             val geocoder = Geocoder(this@MapsActivity, Locale.getDefault())
-             try {
-                 val adresListesi = geocoder.getFromLocation(p0.latitude, p0.longitude, 1)
-                 if (adresListesi.size > 0) {
-                     println(adresListesi.get(0).toString())
-                 }
+                val geocoder = Geocoder( this@MapsActivity, Locale.getDefault())
+                try {
+                    val adresListesi = geocoder.getFromLocation(p0.latitude, p0.longitude, 1)
+                    if (adresListesi.size > 0) {
+                        println(adresListesi.get(0).toString())
+                    }
 
-             } catch (e: Exception) {
-                 e.printStackTrace()
-             }
+                } catch (e: Exception) {
+                    e.printStackTrace()
+                }
 
-         }
+            }
 
-     }
+        }
 
-     if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
-         ActivityCompat.requestPermissions(this, arrayOf(Manifest.permission.ACCESS_FINE_LOCATION), 1)
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this, arrayOf(Manifest.permission.ACCESS_FINE_LOCATION), 1)
 
-     } else {
-         locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 1, 1f, locationListener)
-         val sonBilinenkonum = locationManager.getLastKnownLocation(LocationManager.GPS_PROVIDER)
-         if (sonBilinenkonum != null) {
-             val sonbilinenLatLang = LatLng(sonBilinenkonum.latitude, sonBilinenkonum.longitude)
-             mMap.addMarker(MarkerOptions().position(sonbilinenLatLang).title("Son Bilinen Konumunuz"))
-             mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(sonbilinenLatLang, 12f))
-         }
+        } else {
+            locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 1f, locationListener)
+            val sonBilinenkonum = locationManager.getLastKnownLocation(LocationManager.GPS_PROVIDER)
+            if (sonBilinenkonum != null) {
+                val sonbilinenLatLang = LatLng(sonBilinenkonum.latitude, sonBilinenkonum.longitude)
+                mMap.addMarker(MarkerOptions().position(sonbilinenLatLang).title("Son Bilinen Konumunuz"))
+                mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(sonbilinenLatLang, 12f))
+            }
 
-     }
+        }
 
 
- }
+    }
     fun yenile(red: LatLng){
 // random gelen kordinatlarımıza göre kamerayı hareket ettirip markerın yerini değiştirdiğimiz fonksiyon
 
         mMap.clear()
 
 
-        val denkgelenlokasyon=LatLng(red!!.latitude,red.longitude)
+        val denkgelenlokasyon=LatLng(red.latitude,red.longitude)
         mMap.addMarker(MarkerOptions().position(denkgelenlokasyon).title("Güzel Seçim"))
         mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(denkgelenlokasyon,8f))
         println(denkgelenlokasyon)
@@ -147,7 +147,7 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
         if (requestCode==1){
             if (grantResults.size > 0){
                 if (ContextCompat.checkSelfPermission(this,Manifest.permission.ACCESS_FINE_LOCATION)==PackageManager.PERMISSION_GRANTED){
-                    locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER,1,4f,locationListener)
+                    locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER,0,4f,locationListener)
                 }
             }
         }
@@ -159,7 +159,7 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
         override fun onMapLongClick(p0: LatLng?) {
             mMap.clear()
 
-            val geocoder=Geocoder(this@MapsActivity, Locale.getDefault())
+            val geocoder=Geocoder( this@MapsActivity, Locale.getDefault())
 
             if (p0!=null){
                 var adres = ""
@@ -179,34 +179,45 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
                 }
                 mMap.addMarker(MarkerOptions().position(p0).title(adres))
 
-                val alert=AlertDialog.Builder(this@MapsActivity)
+                val alert=AlertDialog.Builder( this@MapsActivity)
 
                 alert.setTitle("$adres ")
 
                 alert.setMessage("Seçtiğin Yerin Hava Durumunu Görmek İstermisin?")
                 alert.setCancelable(false)
 
-               alert.setPositiveButton("Evet") { dialog ,which ->
-                   var lat =p0.latitude
-                   var log = p0.longitude
+                alert.setPositiveButton("Evet") { dialog ,which ->
+                    val lat =p0.latitude.toString()
+                    val log = p0.longitude.toString()
 
 
-                   var formatlat:Double= String.format("%.2f",lat).toDouble()
-                   var formatlog:Double= String.format("%.2f",log).toDouble()
+                    val formatlat:Double= String.format("%.2f",lat).toDouble()
+                    val formatlog:Double= String.format("%.2f",log).toDouble()
 
-                    var intent =Intent(this@MapsActivity,MainActivity::class.java)
+                    val intent =Intent( this@MapsActivity,MainActivity::class.java)
 
-                    intent.putExtra("lat",formatlat.toString())
-                    intent.putExtra("log",formatlog.toString())
+                    intent.putExtra("lat",formatlat)
+                    intent.putExtra("log",formatlog)
 
-                   println(formatlat)
-                   println(formatlog)
+                 //   lattt=formatlat
+                   // longggg=formatlog
+
+/*
+                    sharedPreferences=applicationContext.getSharedPreferences("com.example.randomweather",Context.MODE_PRIVATE)
+                    var sharedLong= sharedPreferences.getInt("long",0)
+                    var sharedLat= sharedPreferences.getInt("lat",0)
+                    formatlat=sharedLat.toDouble()
+                    formatlog=sharedLong.toDouble()*/
+                    println(formatlat)
+                    println(formatlog)
                     startActivity(intent)
                     finish()
 
+
+
                 }
                 alert.setNegativeButton("Hayır"){ dialog , which ->
-                    Toast.makeText(this@MapsActivity,"Merak hep seni diri tutar.",Toast.LENGTH_LONG).show()
+                    Toast.makeText( this@MapsActivity,"Merak hep seni diri tutar.",Toast.LENGTH_LONG).show()
                 }
                 alert.show()
 
